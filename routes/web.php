9fef29b6d9d0708054e370c4f14877c20ae991ed
ryansyaaw <hazama.ryan1@gmail.com>
2023-11-15 08:17:26 +0700

    project init + criteria table

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::prefix('/')->group (function () {
+    Route::get('/', function () {
+        return view('dashboard.home');
+    })->name('dashboard.home');
+
+    Route::get('/criteria', function () {
+        return view('dashboard.criteria');
+    })->name('dashboard.criteria');
+    Route::get('/user', function () {
+        return view('dashboard.user');
+    })->name('dashboard.user');
+    Route::get('/kandidat', function () {
+        return view('dashboard.kandidat');
+    })->name('dashboard.kandidat');
+});
